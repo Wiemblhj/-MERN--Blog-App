604fa1d11fa0416931c836e414a065fa4365f52e
Wiem <wiembelhaj96@gmail.com>
2023-12-13 23:23:22 +0100

    add posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>
-     test
-    </div>
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="posts">
+        <div className="image">
+   
+          <img src=" https://external.xx.fbcdn.net/emg1/v/t13/6232768099754788528?url=https%3A%2F%2Ftechcrunch.com%2Fwp-content%2Fuploads%2F2023%2F06%2FOpenAI-logo-symmetry.jpg%3Fw%3D850%26h%3D492%26crop%3D1%26fbclid%3DIwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Q&fb_obo=1&utld=techcrunch.com&stp=c0.5000x0.5000f_dst-emg0_p150x78_q75&ccb=13-1&oh=06_AbGsb3WGamSGo4qurhtQhAHHCvmVzpPWOgWfMHpYA2bgiQ&oe=657B752F&_nc_sid=c24604" />
+        </div>
+        <div className="texts">
+          
+          <h2>
+            OpenAI inks deal with Axel Springer on licensing news for model
+            training
+          </h2>
+          <p>
+            Many, if not most, generative AI tech vendors argue that fair use
+            entitles them to train AI models on copyrighted material scraped
+            from the internet — even if they don’t get permission from the
+            rightsholders. But some vendors, such as OpenAI, are hedging their
+            bets — perhaps wary of the outcome of pending relevant lawsuits.
+          </p>
+        </div>
+      </div>
+      <div className="posts">
+        <div className="image">
+          <img src=" https://external.xx.fbcdn.net/emg1/v/t13/6232768099754788528?url=https%3A%2F%2Ftechcrunch.com%2Fwp-content%2Fuploads%2F2023%2F06%2FOpenAI-logo-symmetry.jpg%3Fw%3D850%26h%3D492%26crop%3D1%26fbclid%3DIwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Q&fb_obo=1&utld=techcrunch.com&stp=c0.5000x0.5000f_dst-emg0_p150x78_q75&ccb=13-1&oh=06_AbGsb3WGamSGo4qurhtQhAHHCvmVzpPWOgWfMHpYA2bgiQ&oe=657B752F&_nc_sid=c24604" />
+        </div>
+        <div className="texts">
+          <h2>
+            OpenAI inks deal with Axel Springer on licensing news for model
+            training
+          </h2>
+          <p>
+            Many, if not most, generative AI tech vendors argue that fair use
+            entitles them to train AI models on copyrighted material scraped
+            from the internet — even if they don’t get permission from the
+            rightsholders. But some vendors, such as OpenAI, are hedging their
+            bets — perhaps wary of the outcome of pending relevant lawsuits.
+          </p>
+        </div>
+      </div>
+      <div className="posts">
+        <div className="image">
+        
+          <img src=" https://external.xx.fbcdn.net/emg1/v/t13/6232768099754788528?url=https%3A%2F%2Ftechcrunch.com%2Fwp-content%2Fuploads%2F2023%2F06%2FOpenAI-logo-symmetry.jpg%3Fw%3D850%26h%3D492%26crop%3D1%26fbclid%3DIwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Q&fb_obo=1&utld=techcrunch.com&stp=c0.5000x0.5000f_dst-emg0_p150x78_q75&ccb=13-1&oh=06_AbGsb3WGamSGo4qurhtQhAHHCvmVzpPWOgWfMHpYA2bgiQ&oe=657B752F&_nc_sid=c24604" />
+        </div>
+        <div className="texts">
+          <h2>
+            OpenAI inks deal with Axel Springer on licensing news for model
+            training
+          </h2>
+          <p>
+            Many, if not most, generative AI tech vendors argue that fair use
+            entitles them to train AI models on copyrighted material scraped
+            from the internet — even if they don’t get permission from the
+            rightsholders. But some vendors, such as OpenAI, are hedging their
+            bets — perhaps wary of the outcome of pending relevant lawsuits.
+          </p>
+        </div>
+      </div>
+      <div className="posts">
+        <div className="image">
+
+          <img src=" https://external.xx.fbcdn.net/emg1/v/t13/6232768099754788528?url=https%3A%2F%2Ftechcrunch.com%2Fwp-content%2Fuploads%2F2023%2F06%2FOpenAI-logo-symmetry.jpg%3Fw%3D850%26h%3D492%26crop%3D1%26fbclid%3DIwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Q&fb_obo=1&utld=techcrunch.com&stp=c0.5000x0.5000f_dst-emg0_p150x78_q75&ccb=13-1&oh=06_AbGsb3WGamSGo4qurhtQhAHHCvmVzpPWOgWfMHpYA2bgiQ&oe=657B752F&_nc_sid=c24604" />
+        </div>
+        <div className="texts">
+          <h2>
+            OpenAI inks deal with Axel Springer on licensing news for model
+            training
+          </h2>
+          <p>
+            Many, if not most, generative AI tech vendors argue that fair use
+            entitles them to train AI models on copyrighted material scraped
+            from the internet — even if they don’t get permission from the
+            rightsholders. But some vendors, such as OpenAI, are hedging their
+            bets — perhaps wary of the outcome of pending relevant lawsuits.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
